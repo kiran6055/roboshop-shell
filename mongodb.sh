bdfadfd9b3c5b903bb94ef7002f742a278dfb81a
kiran1616 <kirankumar7163@gmail.com>
2023-01-18 17:15:18 +0530

    secound commit for mongoDB setup

---
+++ mongodb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#author:kiran
+#description: mongoDB automation
+
+script_location=$(pwd)
+
+cp ${script_location}/files/mongoDBrepo /etc/yum.repos.d/mongodb.repo
+
+yum install mongodb-org-y
+systemctl enable mongod
+systemctl start mongod
+
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
+systemctl restart mongod
