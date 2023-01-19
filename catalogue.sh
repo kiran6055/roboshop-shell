358403bcf0ebe00b6e1286c21c918d1d67a248b5
kiran1616 <kirankumar7163@gmail.com>
2023-01-19 22:40:09 +0530

    script for creating frontned automatically

--- catalogue.sh
+++ catalogue.sh
@@ -6,10 +6,11 @@ script_location=$(pwd)
 LOG=/tmp/roboshop.log
 
 status_check() {
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32m Sucess\e[om"
+  if [ $? -eq 0 ]
+  then
+    echo -e "\e[32m Sucess\e[0m"
   else
-    echo -e "\e[31m Failure\e[om"
+    echo -e "\e[31m Failure\e[0m"
     exit
   fi
 }
