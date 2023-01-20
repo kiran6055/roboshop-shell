80398801c1a147ef9d443098fbba42c1712ee112
kiran1616 <kirankumar7163@gmail.com>
2023-01-20 16:37:08 +0530

    script for creating frontned automatically

--- common.sh
+++ common.sh
@@ -21,3 +21,72 @@ print_head() {
 echo -e "\e[1m $1 \e[0m"
 }
 
+Node () {
+print_head settingup node repository
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+status_check
+
+print_head "installing NodeJS"
+yum install nodejs -y &>>${LOG}
+
+status_check
+
+print_head "creating user roboshop"
+id roboshop &>>${LOG}
+if [ $? -ne 0 ]
+then
+  useradd roboshop &>>${LOG}
+fi
+status_check
+
+print_head "creating app"
+mkdir -p /app &>>${LOG}
+status_check
+
+print_head "downloading catalogue code"
+curl -L -o /tmp/${componet}.zip https://roboshop-artifacts.s3.amazonaws.com/${componet}.zip &>>${LOG}
+status_check
+
+print_head "cleanup old content"
+rm -rf /app/* &>>${{LOG}
+status_check
+
+
+
+print_head "unziping catalogue code"
+cd /app
+unzip /tmp/${componet}.zip &>>${LOG}
+status_check
+
+
+print_head "installing NodeJS dependencies"
+cd /app
+npm install &>>${LOG}
+status_check
+
+print_head "configuring catalgoue service file"
+cp ${script_location}/files/${componet}service /etc/systemd/system/${componet}.service &>>${LOG}
+status_check
+
+print_head "system reload"
+systemctl daemon-reload &>>${LOG}
+status_check
+
+print_head "enable catalogue"
+systemctl enable ${componet} &>>${LOG}
+status_check
+
+print_head "tarting catalogue"
+systemctl start ${componet} &>>${LOG}
+status_check
+
+print_head "setting up mongodbrepo config file"
+cp ${script_location}/files/mongoDBrepo /etc/yum.repos.d/mongodb.repo &>>${LOG}
+status_check
+
+print_head "loading Schema"
+yum install mongodb-org-shell -y &>>${LOG}
+status_check
+mongo --host localhost </app/schema/${componet}.js &>>${LOG}
+status_check
+}
