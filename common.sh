078c3e99ed82de5450e9179d52d797b73d90f1fa
kiran1616 <kirankumar7163@gmail.com>
2023-01-21 00:47:08 +0530

    script for creating mysql repo automatically

--- common.sh
+++ common.sh
@@ -21,48 +21,38 @@ print_head() {
 echo -e "\e[1m $1 \e[0m"
 }
 
-Node () {
-print_head "settingup node repository"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
-status_check
 
-print_head "installing NodeJS"
-yum install nodejs -y &>>${LOG}
+app-preq () {
 
-status_check
+  print_head "creating user roboshop"
+  id roboshop &>>${LOG}
+  if [ $? -ne 0 ]
+  then
+    useradd roboshop &>>${LOG}
+  fi
+  status_check
 
-print_head "creating user roboshop"
-id roboshop &>>${LOG}
-if [ $? -ne 0 ]
-then
-  useradd roboshop &>>${LOG}
-fi
-status_check
+  print_head "creating appdirectory"
+  mkdir -p /app &>>${LOG}
+  status_check
 
-print_head "creating app"
-mkdir -p /app &>>${LOG}
-status_check
+  print_head "downloading ${componet} code"
+  curl -L -o /tmp/${componet}.zip https://roboshop-artifacts.s3.amazonaws.com/${componet}.zip &>>${LOG}
+  status_check
 
-print_head "downloading ${componet} code"
-curl -L -o /tmp/${componet}.zip https://roboshop-artifacts.s3.amazonaws.com/${componet}.zip &>>${LOG}
-status_check
-
-print_head "cleanup old content"
-rm -rf /app/* &>>${LOG}
-status_check
+  print_head "cleanup old content"
+  rm -rf /app/* &>>${LOG}
+  status_check
 
 
+  print_head "unziping ${componet} code"
+  cd /app
+  unzip /tmp/${componet}.zip &>>${LOG}
+  status_check
 
-print_head "unziping ${componet} code"
-cd /app
-unzip /tmp/${componet}.zip &>>${LOG}
-status_check
+}
 
-
-print_head "installing NodeJS dependencies"
-cd /app
-npm install &>>${LOG}
-status_check
+systemd_setup () {
 
 print_head "configuring ${componet} service file"
 cp ${script_location}/files/${componet}.service /etc/systemd/system/${componet}.service &>>${LOG}
@@ -76,19 +66,83 @@ print_head "enable ${componet}"
 systemctl enable ${componet} &>>${LOG}
 status_check
 
-print_head "tarting ${componet}"
-systemctl start ${componet} &>>${LOG}
+print_head "restarting ${componet}"
+systemctl restart ${componet} &>>${LOG}
+status_check
+}
+
+
+load_schema () {
+  if [ ${schema_load} == "true" ]; then
+    if [ ${schema_type} == "mongo"]; then
+      print_head "setting up mongodbrepo config file"
+      cp ${script_location}/files/mongoDBrepo /etc/yum.repos.d/mongodb.repo &>>${LOG}
+      status_check
+
+      print_head "installing mongoclient"
+      yum install mongodb-org-shell -y &>>${LOG}
+      status_check
+      print_head "loading schema"
+      mongo --host mongodb-dev.kiranprav.link/app/schema/${componet}.js &>>${LOG}
+      status_check
+    fi
+    if [ ${schema_type} == "mysql"]; then
+      print_head "install mysql client"
+      yum install mysql -y &>>${LOG}
+      status_check
+
+      print_head "load schema"
+      mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -p${root_mysql_password} < /app/schema/${componet}.sql &>>${LOG}
+      status_check
+    fi
+  fi
+
+
+}
+
+
+Node () {
+print_head "settingup node repository"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
 status_check
 
-if [ ${schema_load} == "true" ]; then
-print_head "setting up mongodbrepo config file"
-cp ${script_location}/files/mongoDBrepo /etc/yum.repos.d/mongodb.repo &>>${LOG}
+print_head "installing NodeJS"
+yum install nodejs -y &>>${LOG}
 status_check
 
-print_head "loading Schema"
-yum install mongodb-org-shell -y &>>${LOG}
+app_preq
+
+
+print_head "installing NodeJS dependencies"
+cd /app
+npm install &>>${LOG}
 status_check
-mongo --host mongodb-dev.kiranprav.link/app/schema/${componet}.js &>>${LOG}
-status_check
-fi
+
+systemd_setup
+
+
+load_schema
+
+}
+
+maven () {
+
+  print_head "installing Maven"
+  yum install maven -y &>>${LOG}
+  status_check
+
+  app_preq
+
+  print_head "building Package"
+  mvn clean package &>>${LOG}
+  status_check
+
+  systemd_setup
+
+  print_head "copy app file to app location"
+  mv target/${component}-1.0.jar ${component}.jar
+  status_check
+
+  load_schema
+
 }
--- shipping.sh
+++ shipping.sh
@@ -1,2 +1,11 @@
 source common.sh
 
+if [ -z "${root_mysql_password}" ]; then
+  echo "variable root_mysql_password is required need to mention"
+  exit
+fi
+
+componet=shipping
+schema_load=true
+schema_type=mysql
+maven
