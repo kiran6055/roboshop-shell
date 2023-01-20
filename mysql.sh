ba6cec9ff1f482a548994b1fd9acf3306fa9ed0e
kiran1616 <kirankumar7163@gmail.com>
2023-01-20 23:56:02 +0530

    script for creating mysql repo automatically

---
+++ mysql.sh
@@ -0,0 +1,29 @@
+ source common.sh
+
+if [ -z "$root_mysql_password" ]; then
+  echo "variable root_mysql_password missing"
+  exit 1
+fi
+
+print_head "disabling mysql default version"
+dnf module disable mysql -y &>>${LOG}
+status_check
+
+print_head "copy mysql repo file"
+cp ${script_location}/files/mysql.repo /etc/yum.repos.d/mysql.repo &>>${LOG}
+status_check
+
+print_head "installing mysql"
+yum install mysql-community-server -y &>>${LOG}
+status_check
+
+print_head "enabling and starting Mysql"
+systemctl enable mysqld &>>${LOG}
+systemctl restart mysqld
+status_check
+
+mysql_secure_installation --set-root-pass RoboShop@1 &>>${LOG}
+if [ $? -eq 1 ]; then
+  echo "password is already changed"
+fi
+status_check
