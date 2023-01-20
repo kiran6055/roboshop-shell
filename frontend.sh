3b5fb1c0d639e7b14ddb9bb669568d0dad0373f0
kiran1616 <kirankumar7163@gmail.com>
2023-01-20 22:50:37 +0530

    script for creating frontned automatically

--- frontend.sh
+++ frontend.sh
@@ -1,36 +1,32 @@
-#!/bin/bash
-#author:kiran
-#description:shell script for automation
-
 source common.sh
 
-print_head "install nginx"
+print_head "Install Nginx"
 yum install nginx -y &>>${LOG}
 status_check
 
-systemctl enable nginx &>>${LOG}
-systemctl start nginx &>>${LOG}
-
-print_head "remove old content"
-
+print_head "Remove Nginx Old Content"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
-
 status_check
 
-print_head "Download Frontend content"
 
+print_head "Download Frontend Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
+status_check
+
 cd /usr/share/nginx/html &>>${LOG}
 
-status_check
-
-print_head "unziping frontend contenet in html"
+print_head "Extract Frontend Content"
 unzip /tmp/frontend.zip &>>${LOG}
-
 status_check
 
-print_head "copying roboshop nginxfile"
-cp ${script_location}/files/nginxroboshop.conf /etc/nginx/default.d/roboshop.conf
+print_head "Copy RoboShop Nginx Config File"
+cp ${script_location}/files/nginxroboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
 status_check
 
-systemctl restart  nginx &>>${LOG}
+print_head "Enable Nginx"
+systemctl enable nginx &>>${LOG}
+status_check
+
+print_head "Start Nginx"
+systemctl restart nginx &>>${LOG}
+status_check
