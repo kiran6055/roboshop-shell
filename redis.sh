0a185f45f6ee4c13f882d03b6cad6894c8ff413b
kiran1616 <kirankumar7163@gmail.com>
2023-01-20 14:40:46 +0530

    script for creating frontned automatically

--- redis.sh
+++ redis.sh
@@ -8,7 +8,7 @@ print_head "creating redis repo file"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${LOG}
 status_check
 
-print_head "enableing redis"
+print_head "enabling DNF Redis 6.2 module"
 dnf module enable redis:remi-6.2 -y &>>${LOG}
 status_check
 
@@ -16,7 +16,7 @@ print_head "installing redis"
 yum install redis -y &>>${LOG}
 status_check
 
-print_head "editin g config file with 0.0.0.0"
+print_head "changing listen address to 0.0.0.0"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
 status_check
 
