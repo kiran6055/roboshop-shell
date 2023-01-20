6d7f1dc46b5178a321223f69f171ca6b55a4df99
kiran1616 <kirankumar7163@gmail.com>
2023-01-20 18:08:47 +0530

    script for creating frontned automatically

--- mongodb.sh
+++ mongodb.sh
@@ -5,19 +5,19 @@
 source common.sh
 
 print_head "creating mongorepo"
-cp ${script_location}/files/mongoDBrepo /etc/yum.repos.d/mongodb.repo
+cp ${script_location}/files/mongoDBrepo /etc/yum.repos.d/mongodb.repo &>>${LOG}
 status_check
 
 print_head "installing mongodb"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>${LOG}
 status_check
 
-systemctl enable mongod
-systemctl start mongod
+systemctl enable mongod &>>${LOG}
+systemctl start mongod &>>${LOG}
 
 print_head "changing 127.0.0.1 to 0.0.0.0"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG}
 status_check
 
-systemctl restart mongod
+systemctl restart mongod &>>${LOG}
 status_check
