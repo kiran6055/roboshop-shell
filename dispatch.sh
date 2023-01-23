d0a86c1390c8c9664d2f021bc2f35114ef88b6f0
kiran1616 <kirankumar7163@gmail.com>
2023-01-23 14:55:57 +0530

    shipping.service commit

--- dispatch.sh
+++ dispatch.sh
@@ -1,13 +1,13 @@
 source common.sh
 
+if [ -z "${roboshop_rabbitmq_password}" ]; then
+  echo " variable roboshop_rabbitmq_password is missing"
+  exit 1
+
+fi
 
 component=dispatch
 schema_load=false
 
 golang
 
-if [ -z "${roboshop_rabbitmq_password}" ]; then
-  echo " variable roboshop_rabbitmq_password is missing"
-  exit 1
-
-fi
