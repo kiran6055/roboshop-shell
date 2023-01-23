a55f96f786b0bb124e64fbad5f068d130b39de68
kiran1616 <kirankumar7163@gmail.com>
2023-01-23 13:47:04 +0530

    shipping.service commit

--- payment.sh
+++ payment.sh
@@ -3,7 +3,7 @@ source common.sh
 component=payment
 schema_load=false
 
-if [ -z ${roboshop_rabbitmq_password} ]; then
+if [ -z "${roboshop_rabbitmq_password}" ]; then
   echo "variable roboshop_rabbitmq_password missing"
   exit 1
 fi
