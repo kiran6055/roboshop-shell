5631537f175fc2edcd8c358c0d5ef1f5a913424a
kiran1616 <kirankumar7163@gmail.com>
2023-02-17 00:07:36 +0530

    updated script

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,46 +1,41 @@
 source common.sh
 
 if [ -z "${roboshop_rabbitmq_password}" ]; then
- echo "Variable roboshop_rabbitmq_password_missing"
- exit 1
+  echo "Variable roboshop_rabbitmq_password is missing"
+  exit 1
 fi
 
-print_head "config erlang yum repos"
+print_head "Configuring Erlang YUM Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash &>>${LOG}
 status_check
 
-print_head "installing rlang"
-yum install erlang -y &>>${LOG}
-status_check
-
-print_head "config rabit mq repo"
+print_head "Configuring RabbitMQ YUM Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG}
 status_check
 
-print_head "install rabitmq"
-yum install rabbitmq-server -y &>>${LOG}
+print_head "Install Erlang & RabbitMQ"
+yum install erlang rabbitmq-server -y  &>>${LOG}
 status_check
 
-print_head "enable Rabbitmq server"
-systemctl enable rabbitmq-server &>>${LOG}
+print_head "Enable RabbitMQ Server"
+systemctl enable rabbitmq-server  &>>${LOG}
 status_check
 
-print_head "start server"
-systemctl start rabbitmq-server &>>${LOG}
+print_head "Start RabbitMQ Server"
+systemctl start rabbitmq-server  &>>${LOG}
 status_check
 
-print_head "setting up username and password"
+print_head "Add Application User"
 rabbitmqctl list_users | grep roboshop &>>${LOG}
 if [ $? -ne 0 ]; then
-rabbitmqctl add_user roboshop ${roboshop_rabbitmq_password} &>>${LOG}
+  rabbitmqctl add_user roboshop ${roboshop_rabbitmq_password}  &>>${LOG}
 fi
-
 status_check
 
-print_head "setting up user tagss"
-rabbitmqctl set_user_tags roboshop administrator &>>${LOG}
+print_head "Add Tags to Application User"
+rabbitmqctl set_user_tags roboshop administrator  &>>${LOG}
 status_check
 
-print_head " setting up permission"
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${LOG}
+print_head "Add permission to Application User"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>${LOG}
 status_check
